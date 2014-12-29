782f9ea334cab850e92a79c99888451e5fb80e9a
Filippo Carra <filippo.carra@gmail.com>
2014-12-29 22:33:39 +0100

    Solving pylint warnings and errors to address issue#10

--- default.py
+++ default.py
@@ -1,3 +1,10 @@
+"""
+Kodi (formerly XBMC) addon to bring deejay.it's Reloaded shows on the
+mediacenter.
+This is the main module that takes care of receiving the information from the
+parser and present it in Kodi creating the appropriate pages and feeding the
+URL of the media content to the player.
+"""
 import sys
 import urllib
 import urlparse
@@ -12,112 +19,121 @@ __settings__ = xbmcaddon.Addon(id='plugin.audio.deejayIt.reloaded')
 __language__ = __settings__.getLocalizedString
 
 
-base_url = sys.argv[0]
-addon_handle = int(sys.argv[1])
-args = urlparse.parse_qs(sys.argv[2][1:])
+BASE_URL = sys.argv[0]
+ADDON_HANDLE = int(sys.argv[1])
+ARGS = urlparse.parse_qs(sys.argv[2][1:])
 
-xbmcplugin.setContent(addon_handle, 'episodes')
-xbmcplugin.addSortMethod(addon_handle,
+xbmcplugin.setContent(ADDON_HANDLE, 'episodes')
+xbmcplugin.addSortMethod(ADDON_HANDLE,
     sortMethod=xbmcplugin.SORT_METHOD_UNSORTED)
-xbmcplugin.addSortMethod(addon_handle,
+xbmcplugin.addSortMethod(ADDON_HANDLE,
     sortMethod=xbmcplugin.SORT_METHOD_DATE)
-xbmcplugin.addSortMethod(addon_handle,
+xbmcplugin.addSortMethod(ADDON_HANDLE,
     sortMethod=xbmcplugin.SORT_METHOD_LABEL)
-xbmcplugin.addSortMethod(addon_handle,
+xbmcplugin.addSortMethod(ADDON_HANDLE,
     sortMethod=xbmcplugin.SORT_METHOD_PROGRAM_COUNT)
 
 
 def build_url(query):
-    return base_url + '?' + urllib.urlencode(query)
+    """
+    Taken from http://kodi.wiki/view/Audio/video_add-on_tutorial
+    """
+    return BASE_URL + '?' + urllib.urlencode(query)
 
 
-mode = args.get('mode', None)
+MODE = ARGS.get('mode', None)
 
-if mode is None:
+if MODE is None:
     try:
-        lista = deejay.get_reloaded_list()
-    except IOError as e:    #urllib2 errors are a subclass of IOError
+        LISTA = deejay.get_reloaded_list()
+    #urllib2 errors are a subclass of IOError
+    except IOError as e_urllib2:
         xbmcgui.Dialog().ok(
             __language__(30002),
             __language__(30003),
-            str(e.reason))
+            str(e_urllib2.reason))
     else:
-        for idx, prog in enumerate(lista):
+        for idx, prog in enumerate(LISTA):
             url = build_url({'mode': 'epList',
                              'progName': prog[0],
                              'lastReloadedUrl': prog[2],
                              'showThumb': prog[1]})
-            #showThumbm, parsata da PROGRAMMI, deve essere usata da play -> inoltrata attraverso i modi
+            #showThumb, carried by LISTA, must be used in 'play' mode. It is
+            #thus propagated through the calls
             li = xbmcgui.ListItem(prog[0], iconImage=prog[1])
             li.setInfo('music', {'date': prog[3], 'count': idx})
-            xbmcplugin.addDirectoryItem(handle=addon_handle,
+            xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE,
                                         url=url,
                                         listitem=li,
                                         isFolder=True)
-        xbmcplugin.endOfDirectory(addon_handle)
+        xbmcplugin.endOfDirectory(ADDON_HANDLE)
 
-elif mode[0] == 'epList':
-    progName = args['progName'][0]
-    lastReloadedUrl = args['lastReloadedUrl'][0]
-    showThumb = args['showThumb'][0]
-    fanArt = args.get('fanArt')
+elif MODE[0] == 'epList':
+    PROG_NAME = ARGS['progName'][0]
+    LAST_RELOADED_URL = ARGS['lastReloadedUrl'][0]
+    SHOW_THUMB = ARGS['showThumb'][0]
+    FAN_ART = ARGS.get('fanArt')
     try:
-        episodi, nextpage, img = deejay.get_episodi(url=lastReloadedUrl,
-            oldimg=fanArt)
-    except IOError as e:    #urllib2 errors are a subclass of IOError
+        EPISODI, NEXTPAGE, IMG = deejay.get_episodi(url=LAST_RELOADED_URL,
+            oldimg=FAN_ART)
+    #urllib2 errors are a subclass of IOError
+    except IOError as e_urllib2:
         xbmcgui.Dialog().ok(
             __language__(30002),
             __language__(30004),
-            str(e.reason))
+            str(e_urllib2.reason))
     else:
-        for idx, ep in enumerate(episodi):
-            #('http://www.deejay.it/audio/20071120-2/278354/', '20071120', 'Puntata del 20 Novembre 2007')
-            url = build_url({'mode': 'play',
+        for idx, ep in enumerate(EPISODI):
+            #('http://www.deejay.it/audio/20071120-2/278354/',
+            #   '20071120',
+            #   'Puntata del 20 Novembre 2007')
+            URL = build_url({'mode': 'play',
                              'epUrl': ep[0],
-                             'showThumb': showThumb,
+                             'showThumb': SHOW_THUMB,
                              'title': ep[2],
-                             'progName': progName})
-            li = xbmcgui.ListItem(ep[2],
+                             'progName': PROG_NAME})
+            LI = xbmcgui.ListItem(ep[2],
                                   iconImage='DefaultAudio.png')
-            li.setProperty('IsPlayable', 'true')
+            LI.setProperty('IsPlayable', 'true')
             #Setting fanArt
             #not using setArt to keep Frodo's compatibility
-            #li.setArt({'fanart' : img})
-            li.setProperty('fanart_image', img)
-            li.setInfo('music', {'date': ep[1], 'count': idx})
-            xbmcplugin.addDirectoryItem(handle=addon_handle,
-                                        url=url,
-                                        listitem=li)
+            #LI.setArt({'fanart' : img})
+            LI.setProperty('fanart_image', IMG)
+            LI.setInfo('music', {'date': ep[1], 'count': idx})
+            xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE,
+                                        url=URL,
+                                        listitem=LI)
 
-        if nextpage:
+        if NEXTPAGE:
             #Questo aggiunge la prossima pagina
-            url = build_url({'mode': 'epList',
-                             'progName': progName,
-                             'lastReloadedUrl': nextpage,
-                             'showThumb': showThumb,
-                             'fanArt': img})
-            li = xbmcgui.ListItem('>>> '+__language__(30001)+' >>>')
-            xbmcplugin.addDirectoryItem(handle=addon_handle,
-                                        url=url,
-                                        listitem=li,
+            URL = build_url({'mode': 'epList',
+                             'progName': PROG_NAME,
+                             'lastReloadedUrl': NEXTPAGE,
+                             'showThumb': SHOW_THUMB,
+                             'fanArt': IMG})
+            LI = xbmcgui.ListItem('>>> '+__language__(30001)+' >>>')
+            xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE,
+                                        url=URL,
+                                        listitem=LI,
                                         isFolder=True)
 
         # e chiudiamo la lista
-        xbmcplugin.endOfDirectory(addon_handle)
+        xbmcplugin.endOfDirectory(ADDON_HANDLE)
 
-elif mode[0] == 'play':
+elif MODE[0] == 'play':
     try:
-        url = deejay.get_epfile(args['epUrl'][0])
-    except IOError as e:    #urllib2 errors are a subclass of IOError
+        URL = deejay.get_epfile(ARGS['epUrl'][0])
+    #urllib2 errors are a subclass of IOError
+    except IOError as e_urllib2:
         xbmcgui.Dialog().ok(
             __language__(30002),
             __language__(30005),
-            str(e.reason))
+            str(e_urllib2.reason))
     else:
-        item = xbmcgui.ListItem(path=url)
-        item.setThumbnailImage(args['showThumb'][0])
-        item.setInfo('music', {'title': args['title'][0],
-                               'album': args['progName'][0],
+        LI = xbmcgui.ListItem(path=URL)
+        LI.setThumbnailImage(ARGS['showThumb'][0])
+        LI.setInfo('music', {'title': ARGS['title'][0],
+                               'album': ARGS['progName'][0],
                                'artist': 'Radio Deejay'})
-        xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
-        xbmcplugin.endOfDirectory(addon_handle)
+        xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, LI)
+        xbmcplugin.endOfDirectory(ADDON_HANDLE)
